cb218fb03280a741c951d629f3528d099f509813
Ruslan <foxghost@yandex.ru>
2022-06-07 21:40:09 +0500

    home work 1

--- project/script.js
+++ project/script.js
@@ -1,22 +1,32 @@
-const goods = [
-  { title: 'Shirt', price: 150 },
-  { title: 'Socks', price: 50 },
-  { title: 'Jacket', price: 350 },
-  { title: 'Shoes', price: 250 },
-];
+function init() {
 
-const renderGoodsItem = (title, price) => {
-  return `
-    <div class="goods-item">
-      <h3>${title}</h3>
-      <p>${price}</p>
-    </div>
-  `;
-};
+    const goods = [
+        { title: 'Shirt', price: 150 },
+        { title: 'Socks', price: 50 },
+        { title: 'Jacket', price: 350 },
+        { title: 'Shoes', price: 250 },
+    ];
 
-const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList;
+    const renderGoodsItem = ({ title = 'товар', price = 'н.у' }) => `<div class="goods-item"><h3>${title}</h3><p>Price: ${price}</p></div>`;
+
+    const renderGoodsList = (list = []) => {
+        let goodsList = list.map(item => renderGoodsItem(item)).join('');
+        document.querySelector('.goods-list').innerHTML = goodsList;
+    }
+
+    function styleGoodsList() {
+        let elementsToStyle = document.getElementsByClassName('goods-item')
+        for (let i = 0; i < elementsToStyle.length; i++) {
+            elementsToStyle[i].style.border = "solid 1px salmon";
+            elementsToStyle[i].style.width = "100px";
+            elementsToStyle[i].style.margin = "10px";
+            elementsToStyle[i].style.padding = "5px";
+        }
+    }
+
+    renderGoodsList(goods);
+    styleGoodsList();
+
 }
 
-renderGoodsList(goods);
+window.onload = init
